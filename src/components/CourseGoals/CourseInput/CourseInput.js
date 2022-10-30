6157d9ccb6f4be5cca2ebaea9c5157cd8b26bb28
nikballal <nikhil.ballal1994@gmail.com>
2022-10-30 13:27:28 +1100

    inline-css-styling

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      //once you start typing in input, the error disappears
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    //if form is submitted with empty field (length is 0)
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          type="text"
+          style={{
+            background: !isValid ? "salmon" : "transparent",
+            borderColor: !isValid ? "red" : "black",
+          }}
+          onChange={goalInputChangeHandler}
+        />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
